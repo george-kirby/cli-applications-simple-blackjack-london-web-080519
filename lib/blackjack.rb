48d986775fce83dc025f7ae8313c42ffacc2edb4
George Kirby <georgeskirby@btinternet.com>
2019-07-30 13:24:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,9 +52,9 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   
-  if user_input == 0
+  if user_input == 's'
     card_total
-  elsif user_input == 1
+  elsif user_input == 'h'
     card_total += deal_card
   else
     invalid_command
